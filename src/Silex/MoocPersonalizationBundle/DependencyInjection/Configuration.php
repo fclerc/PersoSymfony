076e0ff6202ff3a71d480b0b6de5029a349c071d
fclerc <fc@florianclerc.com>
2014-09-22 15:41:45 +0200

    First commit, bundle generated

---
+++ src/Silex/MoocPersonalizationBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Silex\MoocPersonalizationBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('silex_mooc_personalization');
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+
+        return $treeBuilder;
+    }
+}
---
+++ src/Silex/MoocPersonalizationBundle/SilexMoocPersonalizationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Silex\MoocPersonalizationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SilexMoocPersonalizationBundle extends Bundle
+{
+}
