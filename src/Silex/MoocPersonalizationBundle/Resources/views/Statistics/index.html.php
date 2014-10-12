5e12823ab62f041da2f25324225f70fd303e41a3
fclerc <fc@florianclerc.com>
2014-10-12 22:23:19 +0200

    Continuing statiistics

--- src/Silex/MoocPersonalizationBundle/Resources/views/Statistics/index.html.php
+++ src/Silex/MoocPersonalizationBundle/Resources/views/Statistics/index.html.php
@@ -1,6 +1,16 @@
 <?php $view->extend('SilexMoocPersonalizationBundle::layout.html.php') ?>
 
+<?php
+
+$lang = $view['request']->getLocale(); 
+
+?>
+
+<?php $view['slots']->start('stylesheets') ?>
+    <link href="css/d3.css" type="text/css" rel="stylesheet"/>
+<?php $view['slots']->stop() ?>
+
+
 <div class="container">
     Hello
-    <?php echo $lang; ?>
 </div>
